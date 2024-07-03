bdf5a404f527001e2dca1551f613616d118e59e9
Thais Bastos <thaisbastos179@gmail.com>
2024-07-03 11:57:05 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,38 @@
 import streamlit as st
 
+def login():
+    st.title("Login")
+    username = st.text_input("Usuário")
+    password = st.text_input("Senha", type="password")
+    login_button = st.button("Login")
+    
+    if login_button:
+        # Verifica se o usuário e senha são válidos (neste exemplo, qualquer coisa é válida)
+        if username != "" and password != "":
+            st.success("Login realizado com sucesso!")
+            return True
+        else:
+            st.error("Usuário ou senha incorretos. Tente novamente.")
+    
+    return False
+
 def main():
-    st.title("Meu Aplicativo Streamlit")
-    
-    # Conteúdo dinâmico da página
-    page = st.sidebar.radio("Selecione uma página", ["Home", "Library", "Tutorials", "Development", "Download"])
-    
-    if page == "Home":
-        st.write("Conteúdo da página Home")
-    elif page == "Library":
-        st.write("Conteúdo da página Library")
-    elif page == "Tutorials":
-        st.write("Conteúdo da página Tutorials")
-    elif page == "Development":
-        st.write("Conteúdo da página Development")
-    elif page == "Download":
-        st.write("Conteúdo da página Download")
+    if login():
+        st.title("Meu Aplicativo Streamlit")
         
+        # Conteúdo dinâmico da página
+        page = st.sidebar.radio("Selecione uma página", ["Home", "Library", "Tutorials", "Development", "Download"])
+        
+        if page == "Home":
+            st.write("Conteúdo da página Home")
+        elif page == "Library":
+            st.write("Conteúdo da página Library")
+        elif page == "Tutorials":
+            st.write("Conteúdo da página Tutorials")
+        elif page == "Development":
+            st.write("Conteúdo da página Development")
+        elif page == "Download":
+            st.write("Conteúdo da página Download")
+
 if __name__ == "__main__":
     main()
